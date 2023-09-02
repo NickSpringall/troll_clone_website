1483780bf7cde3dc9180bfddd7635a1b84fe3111
NickSpringall <123866019+NickSpringall@users.noreply.github.com>
2023-09-02 11:20:40 +1000

    feat whipe the data row

---
+++ script.js
@@ -0,0 +1,15 @@
+
+console.log(trelloData);
+
+
+// Data rendering file
+// Read trelloData, find the data display row and
+// Generate HTML elements based on trelloData
+
+function renderColumns() {
+    let trelloDataRowRootNode = document.getElementById("dataDisplayRow");
+    trelloDataRowRootNode.innerHTML = "";
+
+}
+
+renderColumns();
